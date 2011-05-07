4ffc9ce62f978adf1cf27cbb556a64ee95ec70e5
Adrian Cuadros <adrian@innku.com>
2011-05-07 12:26:12 -0500

    adds rails app

---
+++ 3_rails/proyectos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyectos::Application.config.secret_token = '62ff7436b6189e43fe18d04cc2f80ea85394510072c3d3f897d34afe05d07821194f58f35ddd4660d08100fd1833686ad9c8b725389b4f89a048063ccfed7b2c'
