e7fb750e312c56d2f49fbdde0b357d4aa607c1dc
Adrian Cuadros <adrian@innku.com>
2011-05-07 14:24:42 -0500

    Finishes crud projects

--- 3_rails/proyectos/app/controllers/projects_controller.rb
+++ 3_rails/proyectos/app/controllers/projects_controller.rb
@@ -28,4 +28,10 @@ class ProjectsController < ApplicationController
     redirect_to @project
   end
   
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+  
 end
