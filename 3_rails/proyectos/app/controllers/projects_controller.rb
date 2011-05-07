808665b0f12360c93217f442a05fabf09ea33c8f
Adrian Cuadros <adrian@innku.com>
2011-05-07 14:10:05 -0500

    Adds project info data

--- 3_rails/proyectos/app/controllers/projects_controller.rb
+++ 3_rails/proyectos/app/controllers/projects_controller.rb
@@ -18,4 +18,14 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
   
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    @project.update_attributes(params[:project])
+    redirect_to @project
+  end
+  
 end
